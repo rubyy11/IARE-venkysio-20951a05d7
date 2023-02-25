cea48b0b9398f920e7cae2e4a9ee3e2140df410d
rubyy11 <110048119+rubyy11@users.noreply.github.com>
2023-02-25 11:23:04 +0530

    Add files via upload

---
+++ grp2.py
@@ -0,0 +1,9 @@
+x=int(input("enter the numerator value of fractional part:" ))
+y= int(input("enter the denominator value of fractional part:"))
+print("enter the",x,"/",y,"value of weekly salary")
+m=int(input())
+ws=(m*y)/x
+#print("The 1/4 th value of weekly salary is:",ws)
+z=int(input("Enter the value of z i.e 1/zth value:"))
+ans=(1/z)*ws
+print("The 1/",z,"the value of weekly salary is:",ans)
